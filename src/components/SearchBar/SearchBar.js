4f129b7dd901d481c504c0ee643d334e44d295f7
shadabali604 <shadabali604@gmail.com>
2019-08-07 18:46:41 +0530

    adding search

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,17 +2,39 @@ import React from 'react';
 import './SearchBar.css';
 
 const sortByOptions = {
-       'Best Match': 'best_match',
-        'Highest Rated': 'rating',
-        'Most Reviewed': 'review_count'
-      
+  'Best Match': 'best_match',
+   'Highest Rated': 'rating',
+   'Most Reviewed': 'review_count'
+ 
 }
 class SearchBar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      term: '',
+      location:'',
+      sortBy: 'best_match'
+    }
+     
+  }
+  getSortByClass(sortByOption){
+    if(this.state.sortBy === sortByOption){
+      return 'active';
+    }else{
+      return ''
+    }
+  
+  }
+  handleSortByChange(sortByOption){
+    this.setState({
+    sortBy:sortByOption
+    });
+  }
     renderSortByOptions(){
         
       return Object.keys(sortByOptions).map((sortByOption) => {
       const sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>
+      return <li key={sortByOptionValue} onClick={this.handleSortByChange.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)}>{sortByOption}</li>
          });
     }
 
